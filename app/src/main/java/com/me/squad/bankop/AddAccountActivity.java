07a4011f074f8e3dbe2960ed97935742971d4258
Juan Manuel Rodriguez <juan.m.rodriguez22@gmail.com>
2017-09-15 17:04:36 -0300

    Add account screen UI finished.

--- app/src/main/java/com/me/squad/bankop/AddAccountActivity.java
+++ app/src/main/java/com/me/squad/bankop/AddAccountActivity.java
@@ -2,6 +2,12 @@ package com.me.squad.bankop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.me.squad.bankop.utils.DecimalDigitsInputFilter;
 
 public class AddAccountActivity extends AppCompatActivity {
 
@@ -13,5 +19,17 @@ public class AddAccountActivity extends AppCompatActivity {
         if(getSupportActionBar() != null) {
             getSupportActionBar().setTitle(getString(R.string.add_account_title));
         }
+
+        EditText initialValue = (EditText) findViewById(R.id.initial_value_account);
+        initialValue.setFilters(new InputFilter[] {new DecimalDigitsInputFilter(8,2)});
+
+        Button cancelButton = (Button) findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
